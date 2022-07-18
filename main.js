738bc05ccf65162a642135dfeaa759e7df147b3d
guruprasadDamodare <97739579+guruprasadDamodare@users.noreply.github.com>
2022-07-18 18:08:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+leftwrist=0;
+rightwrist=0;
+difference=0
 function setup(){
     video=createCapture(VIDEO)
     video.position(150,150)
@@ -11,3 +14,15 @@ function modelloaded(){console.log("model is loaded")}
 function gotposes(results){
 if(results.length>0)
 {console.log(results)}}
+function draw(){
+    background("blue")
+fill("darkblue")
+textSize(difference)
+text("guru",70,70)
+leftwrist=results[0].pose.leftWrist.x;
+rightwrist=results[0].pose.rightWrist.x;
+console.log("left wrist ="+leftWrist)
+console.log("left wrist ="+rightWrist)
+difference=floor(leftWrist-rightWrist)
+
+}
